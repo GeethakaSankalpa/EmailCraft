fd70cb6e7a2a79877cdfea40f9ac435c95ef24cb
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-07-25 22:24:36 +0530

    Created main menu for the application

--- EmailApp.java
+++ EmailApp.java
@@ -1,8 +1,27 @@
+import java.util.*;
+
 public class EmailApp {
     public static void main(String[] args){
+        menu();
+//        Email email = new Email("Geethaka", "Sankalpa");
+//        System.out.println(email.showInfo());
 
-        Email email = new Email("Geethaka", "Sankalpa");
-        System.out.println(email.showInfo());
+    }
+
+    public static void menu(){
+        int choice;
+        System.out.println("Welcome to Automatic Email Generator");
+        while(true){
+            System.out.println("1. Enter your first name\n2. Enter your last name\n0. Exit.\n");
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Select your choice: ");
+            try{
+                choice = scan.nextInt();
+                break;
+            }
+            catch (InputMismatchException e){System.out.println("Please check your input.");}
+        }
+
 
     }
 }
